d0461c621b3018f15e9a68bf26c8d35658ea8395
Milad-Aijazi <syedmilad245@gmail.com>
2025-03-01 10:27:32 +0500

    all main components add.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./App.css"
+
+const App = () => {
+  return (
+    <div className='App'>
+        <div className='gradient__bg'>
+              Navbar
+              Header
+        </div>
+    </div>
+  )
+}
+
+export default App
