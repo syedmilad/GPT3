33749b07c51c7899c4f895ee46aa92b8209db47a
Milad-Aijazi <syedmilad245@gmail.com>
2025-03-01 10:37:21 +0500

    assets import & export.

---
+++ src/assets/index.js
@@ -0,0 +1,33 @@
+import ai from './ai.png';
+import atlassian from './atlassian.png';
+import blog01 from './blog01.png';
+import blog02 from './blog02.png';
+import blog03 from './blog03.png';
+import blog04 from './blog04.png';
+import blog05 from './blog05.png';
+import dropbox from './dropbox.png';
+import google from './google.png';
+import logo from './logo.png';
+import logoSvg from './logo.svg';
+import people from './people.png';
+import possibility from './possibility.png';
+import shopify from './shopify.png';
+import slack from './slack.png';
+
+export {
+  ai,
+  atlassian,
+  blog01,
+  blog02,
+  blog03,
+  blog04,
+  blog05,
+  dropbox,
+  google,
+  logo,
+  logoSvg,
+  people,
+  possibility,
+  shopify,
+  slack,
+};
