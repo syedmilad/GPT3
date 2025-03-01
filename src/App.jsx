3e265b79b8f667e59b95ff2dd069aac43886fc6c
Milad-Aijazi <syedmilad245@gmail.com>
2025-03-01 11:48:02 +0500

    completed the navbar sytling.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import "./App.css"
+import { Navbar } from './components'
+import Header from './container/Header/Header'
 
 const App = () => {
   return (
     <div className='App'>
-        <div className='gradient__bg'>
-              Navbar
-              Header
-        </div>
+       <div className='gradient__bg'>
+        <Navbar/>
+        <Header/>
+       </div>
     </div>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
-
+import "./Navbar.css";
+import {logoSvg} from '../../assets';
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='gpt3__navbar' >
+      <div className='gpt3__navbar-links'>
+        <div className='gpt3__navbar-link_logo' >
+            <img src={logoSvg} alt="logo" />
+        </div>
+        <div className='gpt3__navbar-links_container' > 
+            <p><a href="#home">Home</a></p>
+            <p><a href="#wgt3">What is GPT?</a></p>
+            <p><a href="#features">Open AI</a></p>
+            <p><a href="#possibility">Case Studies</a></p>
+            <p><a href="#footer">Library</a></p>
+        </div>
+      </div>
+      <div className='gpt3__navbar-sign'>
+          <p>Sign In</p>
+          <button type='button'>Sign Up</button>
+      </div>
+    </div>
   )
 }
 
