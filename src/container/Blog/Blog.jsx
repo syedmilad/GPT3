d8658172546683772c786bd9f9b6c506cd1b7bcc
Milad-Aijazi <syedmilad245@gmail.com>
2025-03-03 03:22:27 +0500

    updated code.

--- src/container/Blog/Blog.jsx
+++ src/container/Blog/Blog.jsx
@@ -7,7 +7,7 @@ const Blog = () => {
   return (
     <div className='gpt3__blog section__padding' id='blog'>
       <div className='gpt3__blog-heading'>
-        <h1 className='gradient__text'> A lot is happening,
+        <h1 className='gradient__text'> A lot is happening, <br />
           We are blogging about it.</h1>
       </div>
       <div className='gpt3__blog-container'>
