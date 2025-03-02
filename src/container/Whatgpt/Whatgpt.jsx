e5774805f31f431640976141cd53a2812225b954
syedmilad <syedmilad99@gmail.com>
2025-03-03 04:22:20 +0500

    updated code.

--- src/container/Whatgpt/Whatgpt.jsx
+++ src/container/Whatgpt/Whatgpt.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Feature } from '../../components';
 import './whatgpt3.css';
 
-const WhatGPT3 = () => (
+const Whatgpt = () => (
   <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
     <div className='gpt3__whatgpt3-feature'>
       <Feature title="What is GPT-3" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by." />
@@ -19,4 +19,4 @@ const WhatGPT3 = () => (
   </div>
 );
 
-export default WhatGPT3;
+export default Whatgpt;
